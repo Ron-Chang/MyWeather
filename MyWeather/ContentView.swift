f5bd8c7937866f601cdaad9c179abe81f6db8ed4
ron <ron.hsien.chang@gmail.com>
2022-01-14 14:51:50 +0400

    Add main title and image

--- MyWeather/ContentView.swift
+++ MyWeather/ContentView.swift
@@ -14,8 +14,29 @@ struct ContentView: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Dubai,\nUnited Arab Emirates")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("28°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+                
+                Spacer()
+            }
         }
-        
     }
 }
 
